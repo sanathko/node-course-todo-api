0f236958d3e0a03703f9d813edf95be5ebd30aed
sanathkodikara <sanath.kodikara@gmail.com>
2017-03-27 22:37:20 +1100

    add tests for get /todos

--- server/server.js
+++ server/server.js
@@ -12,18 +12,26 @@ app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, e => {
     res.status(400).send(e);
-    console.log('Error is ', e);
   });
-  console.log(req.body);
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, e => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
-  console.log('started on port 3000');
+  console.log('started on port 3000....');
 });
-
 
 module.exports = { app };
