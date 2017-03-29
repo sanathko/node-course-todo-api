b031eccfc7f1b6add5b210df51fd0d428c240603
sanathkodikara <sanath.kodikara@gmail.com>
2017-03-30 08:52:56 +1100

    add delete route

---
+++ playground/mongoose.remove.js
@@ -0,0 +1,12 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+const {ObjectId} = require('mongodb');
+
+Todo.remove({}).then(result => {
+  console.log(result);
+});
+
+Todo.findOneAndRemove().then(result)
+Todo.findByIdAndRemove();
