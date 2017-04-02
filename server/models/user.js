726f1589f220f8563aee3e09af46e8587c19bae9
sanathkodikara <sanath.kodikara@gmail.com>
2017-04-02 16:35:49 +1000

    add seed data for the tests

--- server/models/user.js
+++ server/models/user.js
@@ -71,7 +71,6 @@ UserSchema.pre('save', function(next) {
   if (user.isModified('password')) {
     bcrypt.genSalt(10,(err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
-        console.log(hash);
         user.password = hash;
         next();
       });
